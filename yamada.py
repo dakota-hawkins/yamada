a14307d976c16c056635bd3dd8d81ed9da118fbb
Dakota Hawkins <dakotahawkins0110@gmail.com>
2018-07-10 18:01:11 -0400

    Working class-based implementation for substitute step.

--- yamada.py
+++ yamada.py
@@ -2,6 +2,7 @@ import networkx as nx
 from collections import OrderedDict
 import numpy as np
 from matplotlib import pyplot as plt
+from sortedcontainers import SortedSet
 
 example = {1: {2: {'weight': 2},
                3: {'weight': 1}},
@@ -103,6 +104,25 @@ class Yamada(object):
         if len(set(restricted_edges).intersection(set(tree.edges))) != 0:
             return(False)
         return(True)
+    
+
+class YamadaSubstitute(object):
+    """Class for the substitute algorithm from Yamada et al. 2010."""
+
+    def __init__(self, graph, tree, fixed_edges, restricted_edges,
+                 ordered=False):
+        self.graph = graph
+        self.tree = tree
+        self.fixed_edges = fixed_edges
+        self.restricted_edges = restricted_edges
+        self.ordered = ordered
+
+
+    def instantiate_substitute_variables(self):
+        self.directed = self.tree.to_directed()  # directed graph for postorder
+        self.postorder_nodes, self.descendants = self.postorder_tree()
+        # set Q in original paper
+        self.quasi_cuts = SortedSet(key=lambda x: (x[0], x[1], x[2])) 
 
     def random_node(self, seed=None):
         """Select random node from graph."""
@@ -111,160 +131,146 @@ class Yamada(object):
         r_idx = np.random.randint(0, high=self.graph.number_of_nodes())
         return(list(self.graph.nodes)[r_idx])
 
-    def find_incident_edges(self, tree, node, restricted_edges):
+    def find_incident_edges(self, node):
         """
-        Find all incident edges for a given node not in either the current
-        minimum spanning tree or a set of restricted edges.
+        Find all incident edges for a given node not in the current minimum
+        spanning tree nor a set of restricted edges.
 
         Arguments:
-            tree (nx.Graph): a minimum spanning tree.
             node (int): node of interest.
-            restricted_set (set): set of edges not to consider.
         
         Return:
-            (set): set of incident edges to `node` not contained in
+            (set): set of weighted incident edges to `node` not contained in
                 `restricted_edges`.
         """
         incident_set = set()
         for neighbor in nx.neighbors(self.graph, node):
             edge = (node, neighbor)
-            if edge not in restricted_edges and edge not in tree.edges():
-                incident_set.add(edge)
+            if edge not in self.restricted_edges and edge not in self.tree.edges():
+                w_edge = (self.graph.get_edge_data(*edge)['weight'], *edge)
+                incident_set.add(w_edge)
 
         return incident_set
 
-    def postorder_descendants(self, tree):
+    def postorder_tree(self):
         """
-        Retrieve postorder descendants for all nodes within a tree given a
+        Invoke postorder ordering on all nodes and edges within the tree given a
         random source node.
 
         Reorders a tree in a postorder fashion to retrieve descendants and order
         mappings for all nodes within a tree.
 
         Parameters:
-            tree (nx.graph): minimum spanning tree of an undirected graph.
+            ordered (boolean): whether the nodes in `self.tree` are already in
+                postorder.
         
         Return:
             (dict, dict): tuple of dictionaries. First dictionary maps each node
                 in the graph to its postorder position. The second dictionary
                 serves as a look up for postorder descendants for each node.
         """
-        source_node = self.random_node()
-        # convert to directed graph for descendant identification
-        directed = tree.to_directed()
-        postorder_dict = OrderedDict()
+        nodes = self.tree.nodes
+        if not self.ordered:
+            nodes = nx.dfs_postorder_nodes(self.directed, self.random_node())
+
+        postorder_nodes = OrderedDict()
 
         # map nodes to their postorder position
-        for i, node in enumerate(nx.dfs_postorder_nodes(directed, source_node)):
+        for i, node in enumerate(nodes):
         # for i, node in enumerate(list(self.graph.nodes())):
-            postorder_dict[node] = i
+            postorder_nodes[node] = i + 1
             # remove directed edges not already logged in dictionary
             # --> higher postorder, won't be descendant
             parent_nodes = []
-            for neighbor in nx.neighbors(directed, node):
-                if neighbor not in postorder_dict:
+            for neighbor in nx.neighbors(self.directed, node):
+                if neighbor not in postorder_nodes:
                     parent_nodes.append((node, neighbor))
-            directed.remove_edges_from(parent_nodes)
+            self.directed.remove_edges_from(parent_nodes)
 
         # map nodes to their postordered descendants
-        descendant_dict = {}
-        for each in postorder_dict:
-            descendant_dict[each] = [postorder_dict[each]]
-            for child in nx.descendants(directed, each):
-                descendant_dict[each].append(postorder_dict[child])
-            descendant_dict[each].sort()
+        descendants = {}
+        for each in postorder_nodes:
+            descendants[each] = [postorder_nodes[each]]
+            for child in nx.descendants(self.directed, each):
+                descendants[each].append(postorder_nodes[child])
+            descendants[each].sort()
 
-        return(postorder_dict, descendant_dict)
+        return(postorder_nodes, descendants)
 
-    def get_postordered_edge(self, node, postorder_dict, tree):
-        possible_edges = [(node, x) for x in nx.neighbors(tree, node)]
-        lowest_edge = possible_edges[0]
-        for edge in possible_edges:
-            node_po = postorder_dict[node]
-            edge_po = postorder_dict[edge[1]]
-            if edge_po < postorder_dict[lowest_edge[1]] and edge_po > node_po:
-                lowest_edge = edge
-        return lowest_edge
+    def postordered_edges(self):
+        """Return postorded, weighted edges."""
+        edges = []
+        for u, v in self.directed.edges():
+            w = self.graph.get_edge_data(*(u,v))['weight']
+            edges.append((w, v, u)) 
+        edges = sorted(edges, key=lambda x: (self.postorder_nodes[x[1]],
+                                             self.postorder_nodes[x[2]]))
+        return edges
 
-    def edge_exists(self, node, quasi_cut_set):
-        for weighted_edge in quasi_cut_set:
-            if weighted_edge[1] == node:
-                return True
-        return False
 
-    def equal_weight_descendant(self, weighted_edge, quasi_cuts, postorder_dict, descendant_dict):
+    def equal_weight_descendant(self, weighted_edge):
         weight, node = weighted_edge[0:2]
-        new_edge = None
-        for cut_edge in quasi_cuts:
-            if postorder_dict[cut_edge[1]] in descendant_dict[node]\
-            and cut_edge[0] == weight:
-                new_edge = cut_edge
-        return(new_edge)
+        for cut_edge in self.quasi_cuts:
+            related = self.postorder_nodes[cut_edge[1]] in self.descendants[node]
+            if related and cut_edge[0] == weight:
+                return(cut_edge)
+        return(None)
 
 
-    def substitute(self, tree, fixed_edges, restricted_edges):
+    def substitute(self):
         """
         Find the substitute-set for a given edge in an minimal-spanning tree.
 
         Parameters:
 
         """
-        substitute_dict = dict()
-        for e in tree.edges:
-            substitute_dict[e] = []
-            substitute_dict[e[::-1]]= []
         # step 1
-        quasi_cuts = set()  # set Q in original paper
-        # TODO: ensure lexographical order with respect to w, v
-        postorder_dict, descendant_dict = self.postorder_descendants(tree)
+        self.instantiate_substitute_variables()
+        ordered_edges = self.postordered_edges()
+        substitute_dict = {e[1:]: [] for e in ordered_edges}
+        print(substitute_dict)
+
+        
         # step 2
-        for node in list(postorder_dict)[:-1]:
-            incident_edges = self.find_incident_edges(tree, node,
-                                                      restricted_edges)
+        for n_edge in ordered_edges:
+            node = n_edge[1]
+            incident_edges = self.find_incident_edges(node)
             # step 2.1
-            for edge in incident_edges:
-                weighted_edge = (self.graph.get_edge_data(*edge)['weight'],
-                                 *edge)
-                reversed_edge = (weighted_edge[0], *weighted_edge[1:][::-1])
+            for i_edge in incident_edges:
+                reversed_edge = (i_edge[0], *i_edge[1:][::-1])
 
                 # step 2.1.a 
-                if postorder_dict[edge[1]] < descendant_dict[edge[0]][0]:
-                    if reversed_edge in quasi_cuts:
-                        quasi_cuts.remove(reversed_edge)
-                        quasi_cuts.add(weighted_edge)
+                if self.postorder_nodes[i_edge[2]] < self.descendants[i_edge[1]][0]:
+                    if reversed_edge in self.quasi_cuts:
+                        self.quasi_cuts.remove(reversed_edge)
+                    self.quasi_cuts.add(i_edge)
 
                 # step 2.1.b
-                if postorder_dict[edge[1]] in descendant_dict[edge[0]]:
-                    if reversed_edge in quasi_cuts:
-                        quasi_cuts.remove(reversed_edge)
+                if self.postorder_nodes[i_edge[2]] in self.descendants[i_edge[1]]:
+                    if reversed_edge in self.quasi_cuts:
+                        self.quasi_cuts.remove(reversed_edge)
                 
                 # step 2.1.c
-                if postorder_dict[edge[1]] > descendant_dict[edge[0]][-1]:
-                    quasi_cuts.add(weighted_edge)
+                if self.postorder_nodes[i_edge[2]] > self.descendants[i_edge[1]][-1]:
+                    self.quasi_cuts.add(i_edge)
             
             #step 2.2
-            node_edge = self.get_postordered_edge(node, postorder_dict, tree)
-            weight = tree.get_edge_data(*node_edge)['weight']
-
-            if node_edge not in fixed_edges:
+            if n_edge not in self.fixed_edges:
+                print(n_edge[1], [x for x in self.quasi_cuts])
                 # step 2.2.a
-                cut_edge = self.equal_weight_descendant((weight, *node_edge),
-                                    quasi_cuts, postorder_dict, descendant_dict)
+                cut_edge = self.equal_weight_descendant(n_edge)
 
                 while cut_edge is not None:
-                    print(quasi_cuts)
                     # step 2.2.b
-                    if postorder_dict[cut_edge[2]] in descendant_dict[node]:
-                        quasi_cuts.remove(cut_edge)
+                    if self.postorder_nodes[cut_edge[2]] in self.descendants[node]:
+                        self.quasi_cuts.remove(cut_edge)
                         # back to step 2.2.a
-                        cut_edge = self.equal_weight_descendant((weight, *node_edge),
-                                    quasi_cuts, postorder_dict, descendant_dict)
-                    # step 2.2.c
+                        cut_edge = self.equal_weight_descendant(n_edge)
+                    # step 2.2.c                        substitute_dict[n_edge[1:][::-1]].append(cut_edge[1:])
                     else:
-                        substitute_dict[node_edge].append(cut_edge[1:])
-                        substitute_dict[node_edge[::-1]].append(cut_edge[1:])
+                        substitute_dict[n_edge[1:]].append(cut_edge[1:])
                         cut_edge = None
 
-            # edge_exists(node, quasi_cuts):
         return(substitute_dict)
     
+
